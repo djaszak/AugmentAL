d055bf49c1804fa5dd32df7cc748afb370f3e685
Dennis Jaszak <dennis.jaszak@descript.de>
2024-02-19 10:11:38 +0100

    add augmented_entropy

--- AugmentAL/core/query_strategies.py
+++ AugmentAL/core/query_strategies.py
@@ -1,5 +1,6 @@
 import nlpaug.augmenter.word as naw
 import numpy as np
+from scipy.stats import entropy
 from small_text.query_strategies import (
     ConfidenceBasedQueryStrategy,
     BreakingTies,
@@ -12,16 +13,11 @@ class AugmentedQueryStrategyBase(QueryStrategy):
     which expend other BaseStrategys by using an augmented dataset.
     """
 
-    def __init__(
-        self, base_strategy: QueryStrategy, augmented_indices: dict[int, list[int]] = {}
-    ) -> None:
+    def __init__(self, augmented_indices: dict[int, list[int]] = {}) -> None:
         """Initialize the strategy by providing the augmented_indices for the dataset
-            and the base_strategy that will be used for calculating a score for
-            the samples.
+            and the
 
         Args:
-            base_strategy (QueryStrategy): The strategy that will be used to calculate
-                the scores. Can be any strategy provided by the small_text framework.
             augmented_indices (dict[int, list[int]], optional): A dictionary mapping samples to
                 virtual samples generated based on this one. The key is always one
                 id representing an element from the base set, the value to this
@@ -29,8 +25,10 @@ class AugmentedQueryStrategyBase(QueryStrategy):
                 generated based on the original one. Defaults to {}.
         """
         super().__init__()
-        self.base_strategy = base_strategy
         self.augmented_indices = augmented_indices
+        self.flattened_augmented_values = sorted(
+            {x for v in self.augmented_indices.values() for x in v}
+        )
 
     def get_origin_augmented_index(self, aug_elem_index):
         # TODO: More robust by introducing try except for index error
@@ -46,20 +44,30 @@ class AugmentedExtensionQueryStrategy(AugmentedQueryStrategyBase):
     augmentation to give the base_strategy more samples to score.
     """
 
+    def __init__(
+        self, base_strategy: QueryStrategy, augmented_indices: dict[int, list[int]] = {}
+    ) -> None:
+        """Initialize by adding the base_strategy that will be used for calculating a score for
+            the samples.
+
+        Args:
+            base_strategy (QueryStrategy): base_strategy (QueryStrategy): The strategy that will be used to calculate
+                the scores. Can be any strategy provided by the small_text framework.
+            augmented_indices (dict[int, list[int]], optional): See base class. Defaults to {}.
+        """
+        super().__init__(augmented_indices)
+        self.base_strategy = base_strategy
+
     def query(
         self, clf, dataset, indices_unlabeled, indices_labeled, y, n=10
     ) -> np.ndarray[int]:
         query = self.base_strategy.query(
             clf, dataset, indices_unlabeled, indices_labeled, y, n
         )
-        # Flatten the values. Augmented_indices should be unique mappings.
-        flattened_augmented_values = sorted(
-            {x for v in self.augmented_indices.values() for x in v}
-        )
         original_queried_indices = [
             (
                 self.get_origin_augmented_index(elem)
-                if elem in flattened_augmented_values
+                if elem in self.flattened_augmented_values
                 else elem
             )
             for elem in query
@@ -72,8 +80,30 @@ class AugmentedExtensionQueryStrategy(AugmentedQueryStrategyBase):
         return np.unique(np.array(original_queried_indices + augmented_indices_queried))
 
 
-class AugmentedEntropyQueryStrategy(ConfidenceBasedQueryStrategy):
+class AugmentedEntropyQueryStrategy(
+    AugmentedQueryStrategyBase, ConfidenceBasedQueryStrategy
+):
     """Selects instances which have a big entropy among themselves and
     artifially created instances.
     """
-    pass
+
+    def get_confidence(self, clf, dataset, indices_unlabeled, indices_labeled, y):
+        # Use the best confidence from the classifier
+        proba = clf.predict_proba(dataset).max(axis=1)
+
+        for i in self.augmented_indices:
+            proba[i] = np.mean(
+                np.concatenate(
+                    ([proba[i]], [proba[x] for x in self.augmented_indices[i]])
+                )
+            )
+            for x in self.augmented_indices[i]:
+                proba[x] = proba[i]
+
+        return proba
+
+    def _calculate_entropy(self, proba):
+        return
+
+    def __str__(self):
+        return "AugmentedEntropyQueryStrategy"
